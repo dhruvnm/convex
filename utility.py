8992a84c5f2f5119ed603ebbeb1ce5f9b6c8033e
Dhruv Mehta <dhruvnm@umd.edu>
2020-10-06 14:14:49 -0400

    made orient faster

--- utility.py
+++ utility.py
@@ -12,8 +12,7 @@ def orient(p1, p2, p3):
             -1 if right turn
             0 if no turn.
     """
-    sign, _ =  np.linalg.slogdet([[1, p1[0], p1[1]], [1, p2[0], p2[1]], [1, p3[0], p3[1]]])
-    return sign
+    return np.sign((p2[0] - p1[0])*(p3[1]-p1[1]) - (p2[1] - p1[1])*(p3[0]-p1[0]))
 
 def pseudo_angle(p1, p2):
     """
